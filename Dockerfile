0168f3aada33eb9935aa46f2333bbb6c019f9e92
Raja Sen <rajsen@deloitte.de>
2022-01-06 23:15:08 +0100

    use path prefix

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM python:3.9-slim-bullseye
 
 COPY requirements.txt .
 
-ENV RUN_ID=$RUN_ID
-
 RUN apt-get update && apt-get install -y \
     build-essential \
     libpoppler-cpp-dev \
--- src/variables.py
+++ src/variables.py
@@ -1,8 +1,9 @@
 import os
 
 run_id = os.getenv('RUN_ID', default='')
+path_prefix = os.path.join("/data", run_id)
 
 
-SCHATSI_RUNTIME = f"{run_id}/data/output/schatsi_runtime.csv"
-SCHATSI_STOPWORDS = f"{run_id}/data/params/stopwords.csv"
-SCHATSI_INPUT_FOLDER = "/data/input"
+SCHATSI_RUNTIME = f"{path_prefix}/output/schatsi_runtime.csv"
+SCHATSI_STOPWORDS = f"{path_prefix}/params/stopwords.csv"
+SCHATSI_INPUT_FOLDER = f"{path_prefix}/input"
