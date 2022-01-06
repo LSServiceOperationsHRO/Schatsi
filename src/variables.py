fb4799a904f5ded0b7b813bc4b2a429d806ad152
Raja Sen <rajsen@deloitte.de>
2022-01-06 23:00:21 +0100

    renamed stopword file

--- src/variables.py
+++ src/variables.py
@@ -4,5 +4,5 @@ run_id = os.getenv('RUN_ID', default='')
 
 
 SCHATSI_RUNTIME = f"{run_id}/data/output/schatsi_runtime.csv"
-SCHATSI_STOPWORDS = f"{run_id}/data/params/SCHATSI_stopwords.csv"
+SCHATSI_STOPWORDS = f"{run_id}/data/params/stopwords.csv"
 SCHATSI_INPUT_FOLDER = "/data/input"
